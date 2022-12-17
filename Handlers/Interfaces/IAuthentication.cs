5e85fa38aaabf4f351ee529d6b798de5a49f9015
Kristifor Milchev <kristifor@collaborativesoft.com>
2022-12-17 17:46:57 +0200

    Implementing 2FA account base verification

--- Handlers/Interfaces/IAuthentication.cs
+++ Handlers/Interfaces/IAuthentication.cs
@@ -6,9 +6,11 @@ namespace LoginSample.Handlers;
 
 public interface IAuthentication
 {
+    public string IsPending(ClaimsPrincipal user);
     public string IsAuthenticated(ClaimsPrincipal claims);
+    public Task<bool> CheckConfirmed(HttpContext httpContent, ClaimsPrincipal user, Account account);
     public Task<bool> Authenticate(ClaimsPrincipal User, HttpContext httpContext, Account web3);
-    public Task<bool> SignTwoFactor(TwoFactorRequest request, HttpContext httpContent, ClaimsPrincipal user);
+    public Task<bool> SignTwoFactor(TwoFactorRequest request);
     public bool PairTwoFactor(string email, string message, string signature);
     public Task<bool> Disconnect(HttpContext httpContext);
     public string GenerateSignatureRequest();
